3ef5c499ed3d9b085cc9ef067b49951a14fee074
Jordy Shryock <jordy1611@gmail.com>
2022-08-22 18:30:44 -0600

    Initial Stylings for tiles

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import '../Styles/Styles.scss';
+import React from 'react';
+import Slide from '../Slide/Slide.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Slide />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Slide/Slide.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  const [slide, changeSlide] = useState(false);
+
+  return (
+    <div className='slide slide-from-left'>
+      <div>TEEEEST</div>
+    </div>
+  );
+}
+
+export default App;
